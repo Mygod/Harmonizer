32a346b651801fcd29d3bdce64f80bd5a00f0587
Mygod <mygodstudio@gmail.com>
2014-10-05 17:02:40 +0800

    Favorites (1.1.3.280)

--- Harmonizer/src/main/java/tk/mygod/harmonizer/MainActivity.java
+++ Harmonizer/src/main/java/tk/mygod/harmonizer/MainActivity.java
@@ -1,20 +1,106 @@
 package tk.mygod.harmonizer;
 
-import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.media.AudioFormat;
 import android.media.AudioManager;
 import android.media.AudioTrack;
+import android.os.Build;
 import android.os.Bundle;
-import android.view.MotionEvent;
-import android.view.View;
-import android.widget.EditText;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.view.*;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.*;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 
-public class MainActivity extends Activity {
+public class MainActivity extends ActionBarActivity
+        implements ListView.OnItemClickListener, ListView.OnItemLongClickListener {
+    private static class FavoriteItem {
+        public FavoriteItem(String name, double frequency) {
+            Name = name;
+            Frequency = frequency;
+        }
+
+        public String Name;
+        public double Frequency;
+
+        public String getFullName() {
+            return String.format("%s (%s Hz)", Name, betterToString(Frequency));
+        }
+    }
+    private static class FavoritesAdapter extends ArrayAdapter<FavoriteItem> {
+        public LinkedList<FavoriteItem> Favorites;
+        private SharedPreferences pref;
+
+        private FavoritesAdapter(Context context, LinkedList<FavoriteItem> objects) {
+            super(context, android.R.layout.simple_list_item_1, objects);
+            Favorites = objects;
+        }
+
+        public static FavoritesAdapter createAdapter(Context context) {
+            LinkedList<FavoriteItem> favorites = new LinkedList<FavoriteItem>();
+            FavoritesAdapter result = new FavoritesAdapter(context, favorites);
+            int size = (result.pref = context.getSharedPreferences("favorites", MODE_PRIVATE)).getInt("size", 0);
+            for (int i = 0; i < size; ++i) favorites.add(new FavoriteItem(result.pref.getString(i + "_name", ""),
+                    Double.longBitsToDouble(result.pref.getLong(i + "_freq", 0))));
+            return result;
+        }
+
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) convertView = LayoutInflater.from(getContext())
+                                                        .inflate(android.R.layout.simple_list_item_1, parent, false);
+            FavoriteItem item = getItem(position);
+            ((TextView) convertView.findViewById(android.R.id.text1)).setText(item.getFullName());
+            convertView.setTag(item.Frequency);
+            return convertView;
+        }
+
+        @Override
+        public void notifyDataSetChanged() {
+            super.notifyDataSetChanged();
+            int oldSize = pref.getInt("size", 0), size = 0;
+            SharedPreferences.Editor editor = pref.edit();
+            for (FavoriteItem favorite : Favorites) {
+                editor.putString(size + "_name", favorite.Name);
+                editor.putLong(size++ + "_freq", Double.doubleToLongBits(favorite.Frequency));
+            }
+            editor.putInt("size", size);
+            while (size < oldSize) {
+                editor.remove(size + "_name");
+                editor.remove(size++ + "_freq");
+            }
+            if (Build.VERSION.SDK_INT >= 9) editor.apply();
+            else editor.commit();
+        }
+    }
+
     private double savedFrequency;
     private AudioTrack savedTrack, muteTrack;
     private boolean pressed;
+    private EditText frequencyText;
+    private FavoritesAdapter favoritesAdapter;
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle drawerToggle;
+    private MenuItem addFavoriteMenu;
+
+    private static String betterToString(double value) {
+        return value == Math.floor(value) ? String.format("%.0f", value) : Double.toString(value);
+    }
+    private double getFrequency() {
+        try {
+            return Double.parseDouble(frequencyText.getText().toString());
+        } catch (Exception exc) {
+            return 1000;
+        }
+    }
 
     private static AudioTrack generateTrack(double frequency)
     {
@@ -45,6 +131,20 @@ public class MainActivity extends Activity {
         if (savedTrack != null) savedTrack.pause();
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        muteTrack.play();   // prevent start_output_stream delay
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        muteTrack.pause();
+        stop();
+    }
+
+    @Override
     protected void onCreate(Bundle bundle) {
         super.onCreate(bundle);
         setContentView(R.layout.activity_main);
@@ -52,21 +152,40 @@ public class MainActivity extends Activity {
                                    AudioFormat.ENCODING_PCM_16BIT, 32, AudioTrack.MODE_STATIC);
         muteTrack.write(new short[16], 0, 16);
         muteTrack.setLoopPoints(0, 16, -1);
+        frequencyText = (EditText) findViewById(R.id.frequency_text);
+        (drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout))
+                .setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
+        drawerLayout.setDrawerListener(drawerToggle = new ActionBarDrawerToggle
+                (this, drawerLayout, R.drawable.ic_drawer, R.string.drawer_open, R.string.drawer_close) {
+            @Override
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                getSupportActionBar().setTitle(R.string.app_name);
+                addFavoriteMenu.setVisible(false);
+            }
+
+            @Override
+            public void onDrawerOpened(View view) {
+                super.onDrawerOpened(view);
+                getSupportActionBar().setTitle(R.string.favorites);
+                addFavoriteMenu.setVisible(true);
+            }
+        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        ListView favoriteList = (ListView) findViewById(R.id.favorite);
+        favoriteList.setEmptyView(findViewById(android.R.id.empty));
+        favoriteList.setAdapter(favoritesAdapter = FavoritesAdapter.createAdapter(this));
+        favoriteList.setOnItemClickListener(this);
+        favoriteList.setOnItemLongClickListener(this);
         findViewById(R.id.beep_button).setOnTouchListener(new View.OnTouchListener() {
             public boolean onTouch(View view, MotionEvent motionevent) {
                 switch (motionevent.getAction()) {
                     case MotionEvent.ACTION_DOWN:
                         pressed = true;
                         (new Thread() {
-                            private double frequency;
-
                             public void run() {
-                                try {
-                                    frequency = Double.parseDouble(((EditText) findViewById(R.id.frequencyText))
-                                            .getText().toString());
-                                } catch (Exception exc) {
-                                    frequency = 1000;
-                                }
+                                double frequency = getFrequency();
                                 if (savedFrequency != frequency) {
                                     savedFrequency = frequency;
                                     if (savedTrack != null) {
@@ -90,15 +209,80 @@ public class MainActivity extends Activity {
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
-        muteTrack.play();   // prevent start_output_stream delay
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        drawerToggle.syncState();
     }
 
     @Override
-    protected void onPause() {
-        super.onPause();
-        muteTrack.pause();
-        stop();
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.favorites, menu);
+        (addFavoriteMenu = menu.findItem(R.id.add_to_favorites))
+                .setVisible(drawerLayout.isDrawerOpen(GravityCompat.START));
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (drawerToggle.onOptionsItemSelected(item)) return true;
+        if (item.getItemId() != R.id.add_to_favorites) return super.onOptionsItemSelected(item);
+        final EditText text = new EditText(this);
+        new AlertDialog.Builder(this).setTitle(R.string.add_favorite_dialog_title).setView(text)
+                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        favoritesAdapter.add(new FavoriteItem(text.getText().toString(), getFrequency()));
+                    }
+                }).setNegativeButton(android.R.string.cancel, null).show();
+        text.requestFocus();
+        ((InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE))
+                .toggleSoftInput(InputMethodManager.SHOW_FORCED, 0);
+        return true;
+    }
+
+    /**
+     * Callback method to be invoked when an item in this AdapterView has
+     * been clicked.
+     * <p/>
+     * Implementers can call getItemAtPosition(position) if they need
+     * to access the data associated with the selected item.
+     *
+     * @param parent   The AdapterView where the click happened.
+     * @param view     The view within the AdapterView that was clicked (this
+     *                 will be a view provided by the adapter)
+     * @param position The position of the view in the adapter.
+     * @param id       The row id of the item that was clicked.
+     */
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Object tag = view.getTag();
+        if (tag instanceof Double) frequencyText.setText(betterToString((Double) tag));
+        drawerLayout.closeDrawers();
+    }
+
+    /**
+     * Callback method to be invoked when an item in this view has been
+     * clicked and held.
+     * <p/>
+     * Implementers can call getItemAtPosition(position) if they need to access
+     * the data associated with the selected item.
+     *
+     * @param parent   The AbsListView where the click happened
+     * @param view     The view within the AbsListView that was clicked
+     * @param position The position of the view in the list
+     * @param id       The row id of the item that was clicked
+     * @return true if the callback consumed the long click, false otherwise
+     */
+    @Override
+    public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+        favoritesAdapter.remove(favoritesAdapter.getItem(position));
+        return false;
     }
 }
