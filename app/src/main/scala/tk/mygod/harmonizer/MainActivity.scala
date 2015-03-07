74c692e9be4255bb5a4666b271d5af98782bb64f
Mygod <mygodstudio@gmail.com>
2015-03-07 15:38:43 +0800

    Customizable sampling rate, YAY

--- app/src/main/scala/tk/mygod/harmonizer/MainActivity.scala
+++ app/src/main/scala/tk/mygod/harmonizer/MainActivity.scala
@@ -13,21 +13,23 @@ import android.support.v7.widget.{DefaultItemAnimator, LinearLayoutManager, Recy
 import android.view.View.OnTouchListener
 import android.view._
 import android.view.inputmethod.InputMethodManager
-import android.widget.{EditText, TextView}
+import android.widget.{NumberPicker, EditText, TextView}
 
 import scala.collection.mutable.ArrayBuffer
 
 final class MainActivity extends Activity with OnMenuItemClickListener {
   var buffer: ArrayBuffer[Short] = null // recycling ArrayBuffer ;-)
 
-  private def generateTrack(frequency: Double): AudioTrack = {
-    var max = if (frequency <= 0) 2 else (48000 / frequency).toInt
-    if (max < 16) max = 16 else if (max > 2880000) max = 2880000
+  private def generateTrack(frequency: Double, samplingRate: Int): AudioTrack = {
+    val s10 = samplingRate * 10
+    val minute = samplingRate * 60
+    var max = if (frequency <= 0) 2 else (samplingRate / frequency).toInt
+    if (max < 16) max = 16 else if (max > minute) max = minute
     if (buffer == null) buffer = new ArrayBuffer[Short](max) else buffer.sizeHint(max)
-    val k = 3.14159265358979323846264338327950288 / 24000 * frequency
+    val k = 3.14159265358979323846264338327950288 * 2 * frequency / samplingRate
     var i = 0
     var last: Short = 32767
-    while (i < max || i < 524288 && last < 32767 || i < 2880000 && last < 32760) {  // 1M, NO MORE THAN 60s
+    while (i < max || i < 524288 && last < 32767 || i < s10 && last < 32760) {
       last = (32767 * Math.cos(k * i)).round.toShort
       buffer.append(last)
       i += 1
@@ -45,7 +47,7 @@ final class MainActivity extends Activity with OnMenuItemClickListener {
       s += 1
     }
     buffer.clear
-    val track = new AudioTrack(AudioManager.STREAM_MUSIC, 48000, AudioFormat.CHANNEL_OUT_MONO,
+    val track = new AudioTrack(AudioManager.STREAM_MUSIC, samplingRate, AudioFormat.CHANNEL_OUT_MONO,
                                AudioFormat.ENCODING_PCM_16BIT, i << 1, AudioTrack.MODE_STATIC)
     track.write(samples, 0, i)
     track.setLoopPoints(0, i, -1)
@@ -129,12 +131,14 @@ final class MainActivity extends Activity with OnMenuItemClickListener {
   }
 
   private var savedFrequency = .0
+  private var savedSamplingRate = 0
   private var savedTrack: AudioTrack = null
   private val muteTrack = new AudioTrack(AudioManager.STREAM_MUSIC, 48000, AudioFormat.CHANNEL_OUT_MONO,
                                          AudioFormat.ENCODING_PCM_16BIT, 32, AudioTrack.MODE_STATIC)
   muteTrack.write(new Array[Short](16), 0, 16)
   muteTrack.setLoopPoints(0, 16, -1)
   private lazy val frequencyText = findViewById(R.id.frequency_text).asInstanceOf[EditText]
+  private var samplingRatePicker: NumberPicker = _
   private lazy val favoritesAdapter = new FavoritesAdapter
   private lazy val drawerLayout = findViewById(R.id.drawer_layout).asInstanceOf[DrawerLayout]
   private var drawerToggle: ActionBarDrawerToggle = null
@@ -203,6 +207,25 @@ final class MainActivity extends Activity with OnMenuItemClickListener {
       override def onTouch(v: View, event: MotionEvent): Boolean = false
     })
     favoriteList.setAdapter(favoritesAdapter)
+    samplingRatePicker = findViewById(R.id.sampling_rate_picker).asInstanceOf[NumberPicker]
+    try {
+      val minField = classOf[AudioTrack].getDeclaredField("SAMPLE_RATE_HZ_MIN")
+      minField.setAccessible(true)
+      samplingRatePicker.setMinValue(minField.get(null).asInstanceOf[Int])
+    } catch {
+      case exc: NoSuchFieldException => samplingRatePicker.setMinValue(4000)
+    }
+    try {
+      val maxField = classOf[AudioTrack].getDeclaredField("SAMPLE_RATE_HZ_MAX")
+      maxField.setAccessible(true)
+      val max = maxField.get(null).asInstanceOf[Int]
+      samplingRatePicker.setMaxValue(max)
+      samplingRatePicker.setValue(max)
+    } catch {
+      case exc: NoSuchFieldException => // 48000 are used before this field is introduced
+        samplingRatePicker.setMaxValue(48000)
+        samplingRatePicker.setValue(48000)
+    }
     findViewById(R.id.beep_button).setOnTouchListener(new OnTouchListener {
       override def onTouch(v: View, event: MotionEvent): Boolean = {
         event.getAction match {
@@ -211,14 +234,16 @@ final class MainActivity extends Activity with OnMenuItemClickListener {
             new Thread {
               override def run {
                 val frequency = getFrequency
-                if (savedFrequency != frequency) {
+                val samplingRate = samplingRatePicker.getValue
+                if (savedFrequency != frequency || savedSamplingRate != samplingRate) {
                   savedFrequency = frequency
+                  savedSamplingRate = samplingRate
                   if (savedTrack != null) {
                     savedTrack.stop
                     savedTrack.release
                     savedTrack = null
                   }
-                  savedTrack = generateTrack (frequency)
+                  savedTrack = generateTrack(frequency, samplingRate)
                 }
                 if (savedTrack != null && pressed) savedTrack.play
               }
