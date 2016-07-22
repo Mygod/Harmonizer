d52f11719a11c2a7eec49aa5546681dc99ceb27b
Mygod <mygodstudio@gmail.com>
2016-07-22 22:25:36 +0800

    Add detection for near ultrasound

--- src/main/scala/tk/mygod/harmonizer/AudioConfig.scala
+++ src/main/scala/tk/mygod/harmonizer/AudioConfig.scala
@@ -2,13 +2,16 @@ package tk.mygod.harmonizer
 
 import android.content._
 import android.media.{AudioFormat, AudioManager, AudioTrack}
-import tk.mygod.content.ContextPlus
+import android.util.Log
+import android.widget.Toast
 import tk.mygod.os.Build
 
 /**
  * @author Mygod
  */
 object AudioConfig extends BroadcastReceiver with SharedPreferences.OnSharedPreferenceChangeListener {
+  private final val TAG = "AudioConfig"
+
   private var preferences: SharedPreferences = _
   var sampleRate: Int = _
   var format: Int = _
@@ -16,7 +19,7 @@ object AudioConfig extends BroadcastReceiver with SharedPreferences.OnSharedPref
   private var formatChanged = true
   private var initialized: Boolean = _
 
-  def init(context: ContextPlus) = if (!initialized) {
+  def init(context: Context) = if (!initialized) {
     preferences = context.getSharedPreferences("settings", Context.MODE_PRIVATE)
     preferences.registerOnSharedPreferenceChangeListener(this)
     val filter = new IntentFilter(AudioManager.ACTION_AUDIO_BECOMING_NOISY)
@@ -24,11 +27,27 @@ object AudioConfig extends BroadcastReceiver with SharedPreferences.OnSharedPref
     filter.addAction(AudioManager.ACTION_HEADSET_PLUG)
     filter.addAction(AudioManager.ACTION_SCO_AUDIO_STATE_UPDATED)
     sampleRate = AudioTrack.getNativeOutputSampleRate(AudioManager.STREAM_MUSIC)
+    onReceive(context, null)
     context.registerReceiver(this, filter)
     initialized = true
   }
 
-  override def onReceive(context: Context, intent: Intent): Unit = sampleRateChanged = true
+  override def onReceive(context: Context, intent: Intent) {
+    sampleRateChanged = true
+    val am = context.getSystemService(Context.AUDIO_SERVICE).asInstanceOf[AudioManager]
+    if (Build.version >= 23) {
+      var failed = true
+      if (am.getProperty(AudioManager.PROPERTY_SUPPORT_MIC_NEAR_ULTRASOUND).toBoolean) {
+        Log.i(TAG, "The default microphone audio source supports near-ultrasound frequencies (range of 18 - 21 kHz).")
+        failed = false
+      }
+      if (am.getProperty(AudioManager.PROPERTY_SUPPORT_SPEAKER_NEAR_ULTRASOUND).toBoolean) {
+        Log.i(TAG, "The default speaker audio path supports near-ultrasound frequencies (range of 18 - 21 kHz).")
+        failed = false
+      }
+      if (failed) Toast.makeText(context, R.string.near_ultrasound_unsupported, Toast.LENGTH_LONG).show()
+    }
+  }
   def onSharedPreferenceChanged(pref: SharedPreferences, key: String) = formatChanged = true
   def changed = {
     var result = false
