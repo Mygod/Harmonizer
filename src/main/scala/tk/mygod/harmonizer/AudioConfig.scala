71e5e37544c247e83fa7a81ed7be0c8ea6ec2dff
Mygod <mygodstudio@gmail.com>
2016-08-02 10:42:49 -0700

    Fix bug for setting preferences

--- src/main/scala/tk/mygod/harmonizer/AudioConfig.scala
+++ src/main/scala/tk/mygod/harmonizer/AudioConfig.scala
@@ -9,24 +9,28 @@ import tk.mygod.os.Build
 /**
  * @author Mygod
  */
-object AudioConfig extends BroadcastReceiver with SharedPreferences.OnSharedPreferenceChangeListener {
+object AudioConfig extends BroadcastReceiver {
   private final val TAG = "AudioConfig"
+  final val BIT_DEPTH = "audio.bitDepth"
 
   private var preferences: SharedPreferences = _
   var sampleRate: Int = _
   var format: Int = _
   private var sampleRateChanged: Boolean = _
-  private var formatChanged = true
+  var formatChanged = true
   private var initialized: Boolean = _
+
+  def getFormat = preferences.getString(BIT_DEPTH, (if (Build.version >= 21)
+    AudioFormat.ENCODING_PCM_FLOAT else AudioFormat.ENCODING_PCM_16BIT).toString).toInt
 
   def init(context: Context) = if (!initialized) {
     preferences = context.getSharedPreferences("settings", Context.MODE_PRIVATE)
-    preferences.registerOnSharedPreferenceChangeListener(this)
     val filter = new IntentFilter(AudioManager.ACTION_AUDIO_BECOMING_NOISY)
     filter.addAction(AudioManager.ACTION_HDMI_AUDIO_PLUG)
     filter.addAction(AudioManager.ACTION_HEADSET_PLUG)
     filter.addAction(AudioManager.ACTION_SCO_AUDIO_STATE_UPDATED)
     sampleRate = AudioTrack.getNativeOutputSampleRate(AudioManager.STREAM_MUSIC)
+    format = getFormat
     onReceive(context, null)
     context.registerReceiver(this, filter)
     initialized = true
@@ -48,12 +52,10 @@ object AudioConfig extends BroadcastReceiver with SharedPreferences.OnSharedPref
       if (failed) Toast.makeText(context, R.string.near_ultrasound_unsupported, Toast.LENGTH_LONG).show()
     }
   }
-  def onSharedPreferenceChanged(pref: SharedPreferences, key: String) = formatChanged = true
   def changed = {
     var result = false
     if (formatChanged) {
-      val newValue = preferences.getString("audio.bitDepth", (if (Build.version >= 21)
-        AudioFormat.ENCODING_PCM_FLOAT else AudioFormat.ENCODING_PCM_16BIT).toString).toInt
+      val newValue = getFormat
       if (newValue != format) {
         format = newValue
         result = true
--- src/main/scala/tk/mygod/harmonizer/SettingsFragment.scala
+++ src/main/scala/tk/mygod/harmonizer/SettingsFragment.scala
@@ -1,15 +1,20 @@
 package tk.mygod.harmonizer
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import tk.mygod.preference.{DropDownPreference, PreferenceFragmentPlus}
 
 /**
  * @author Mygod
  */
-final class SettingsFragment extends PreferenceFragmentPlus {
+final class SettingsFragment extends PreferenceFragmentPlus with SharedPreferences.OnSharedPreferenceChangeListener {
   def onCreatePreferences(savedInstanceState: Bundle, rootKey: String) {
-    getPreferenceManager.setSharedPreferencesName("settings")
+    val pm = getPreferenceManager
+    pm.setSharedPreferencesName("settings")
+    pm.getSharedPreferences.registerOnSharedPreferenceChangeListener(this)
     addPreferencesFromResource(R.xml.settings)
-    findPreference("audio.bitDepth").asInstanceOf[DropDownPreference].setValue(AudioConfig.format.toString)
+    findPreference(AudioConfig.BIT_DEPTH).asInstanceOf[DropDownPreference].setValue(AudioConfig.format.toString)
   }
+
+  def onSharedPreferenceChanged(pref: SharedPreferences, key: String) = AudioConfig.formatChanged = true
 }
